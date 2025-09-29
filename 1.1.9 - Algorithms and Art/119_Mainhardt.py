a89821e3a07b611b4498e4a93863dc8af7a703a4
VyPhan-27 <305681@shakopeeschools.org>
2025-09-29 09:24:14 -0500

    Done so far
    Added background

---
+++ 1.1.9 - Algorithms and Art/119_Mainhardt.py
@@ -0,0 +1,27 @@
+import turtle as trtl
+
+screen = trtl.Screen()
+screen.bgcolor("black") # Set the background color to black
+
+#list
+my_planets = ["Mercury", "Venus", "Earth", "Mars", "Jupter", "Saturn", "Uranus", "Neptune"] #planets list
+redo = "back?"
+
+#user input
+
+for planets in my_planets:
+
+
+
+
+#custom defintion
+ trtl.addshape("star", ((0, 3), (2, 0), (4,-1), (2,-2), (3,-4), (1,-3), (-1,-3), (-3,-4), (-2,-2), (-4, -1), (-2, 0)))
+
+
+
+#iteration
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
