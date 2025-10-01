39325e216db618fc7fb2cb3924a1a9c5be07b0af
VyPhan-27 <305681@shakopeeschools.org>
2025-10-01 09:15:48 -0500

    Update 119_Mainhardt.py
    start to organized my codes

--- 1.1.9 - Algorithms and Art/119_Mainhardt.py
+++ 1.1.9 - Algorithms and Art/119_Mainhardt.py
@@ -3,29 +3,36 @@ import turtle as trtl
 screen = trtl.Screen()
 screen.bgcolor("black") # Set the background color to black
 
-#input texts
-my_planets = trtl.textinput("planets", "Do you want to see the planets? y/n")
-if my_planets == "y":
- 
-
-
-#list
- my_planets = ["Mercury", "Venus", "Earth", "Mars", "Jupter", "Saturn", "Uranus", "Neptune"] #planets list
-my_planets_colors = ["lightgrey", "darkorange", "royalblue", "crimson", "goldenrod", "khaki", "lightskyblue", "dodgerblue"]
-
-#user input
-
-for planets in my_planets:
-
-
 
 
 #custom defintion
- trtl.addshape("star", ((0, 3), (2, 0), (4,-1), (2,-2), (3,-4), (1,-3), (-1,-3), (-3,-4), (-2,-2), (-4, -1), (-2, 0)))
+trtl.addshape("star", ((0, 3), (2, 0), (4,-1), (2,-2), (3,-4), (1,-3), (-1,-3), (-3,-4), (-2,-2), (-4, -1), (-2, 0)))
 
 
+#user texts
+other_planets = trtl.textinput("Planets", "Do you want to see the planets? y/n")
+if other_planets == "y":
+
+
+
+ 
+#list
+ my_planets = ["Mercury", "Venus", "Earth", "Mars", "Jupter", "Saturn", "Uranus", "Neptune"] #planets list
+ my_planets_colors = ["grey", "orange", "blue", "darkred", "yellow", "gold", "cyan", "dodgerblue"]
 
 #iteration
+
+tloc = 10
+for planets in my_planets:
+
+ planets = trtl.Turtle()
+ planets.circle(90)
+ planets.fillcolor(my_planets_colors)
+ planets.penup()
+ planets.goto(-10, tloc)
+ planets.setheading(0)
+
+
 
 
 
