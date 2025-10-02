323b198799e01e619b39dca6b144200b573bdd93
VyPhan-27 <305681@shakopeeschools.org>
2025-10-02 08:51:06 -0500

    Update 119_Mainhardt.py
    i did some changes to the direction of my planets and also the size of it to match the real life planets

--- 1.1.9 - Algorithms and Art/119_Mainhardt.py
+++ 1.1.9 - Algorithms and Art/119_Mainhardt.py
@@ -8,15 +8,15 @@ screen.bgcolor("black")  # Set background color to black
 trtl.addshape("star", ((0, 3), (2, 0), (4, -1), (2, -2), (3, -4), (1, -3), (-1, -3), (-3, -4), (-2, -2), (-4, -1), (-2, 0)))
 
 # Draw a star (e.g., representing the sun)
-sun = trtl.Turtle()
-sun.pensize(4)
-sun.shape("star")
-sun.fillcolor("yellow")
-sun.penup()
-sun.goto(200, 0)  # Position the star to the right
-sun.begin_fill()
-sun.stamp()  # Stamp the star shape
-sun.end_fill()
+star = trtl.Turtle()
+star.pensize(4)
+star.shape("star")
+star.fillcolor("yellow")
+star.penup()
+star.goto(200, 0)  # Position the star to the right
+star.begin_fill()
+star.stamp()  # Stamp the star shape
+star.end_fill()
 
 # User input
 other_planets = trtl.textinput("Planets", "Do you want to see the planets? y/n")
@@ -27,22 +27,27 @@ my_planets_colors = ["lightgrey", "darkorange", "royalblue", "crimson", "goldenr
 
 
 # Plant radius
-planet_radii = [20, 25, 28, 22, 35, 32, 30, 39]
+planet_radii = [10, 15, 15, 15, 26, 24, 20, 20]
 
 # Draw planets if user selects 'y'
 if other_planets.lower() == "y":
-    tloc = 150  # Starting y-coordinate
+    tloc = 250  # Starting y-coordinate
     for i in range(len(my_planets)):
         planet = trtl.Turtle()
         planet.shape("star")  # Use circle for planets
-        planet.fillcolor(my_planets_colors[i])  # Set color from list
+        planet.fillcolor(my_planets_colors[i]) # Set color from list
         planet.penup()
-        planet.goto(-150, tloc)  # Position planet
+        planet.goto(-200, tloc) # Position planet
+        planet.setheading(0)
         planet.pendown()
         planet.begin_fill()
-        planet.circle(20)  # Smaller radius for visibility
+        planet.circle(planet_radii[i])  # Smaller radius for visibility
         planet.end_fill()
-        tloc -= (planet_radii[i] * 2 + 10)  # Move down for next planet
+        tloc -= (planet_radii[i] * 4 + 9)  # Move down for next planet
+else:
+    exit()
+
+
 
 # Keep the window open
 screen.mainloop()
