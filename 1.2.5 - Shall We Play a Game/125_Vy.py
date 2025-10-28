980e091e825ecc1aa2f8a062c4358b7a2dc1b774
VyPhan-27 <305681@shakopeeschools.org>
2025-10-28 08:52:46 -0500

    Milestone
    I listed down all the milestone

--- 1.2.5 - Shall We Play a Game/125_Vy.py
+++ 1.2.5 - Shall We Play a Game/125_Vy.py
@@ -1,1 +1,11 @@
 # TODO create an Title Screen
+
+# TODO Create Position for each cars
+
+# TODO Customize Turtle 
+
+# TODO Create an timer 
+
+# TODO Win or Lose Screen
+
+# TODO Players Scores and Difficulty level chosen
