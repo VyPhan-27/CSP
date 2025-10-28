b23ed6c6320f42565748f5e5e4480019ad0a128d
VyPhan-27 <305681@shakopeeschools.org>
2025-10-28 08:25:06 -0500

    Title screen
    I started to add a GIF for some of my background designs, and also started on making an title for my games

---
+++ 1.2.5 - Shall We Play a Game/125_Mainhardt.py
@@ -0,0 +1,28 @@
+import turtle as trtl
+import random as rand
+
+
+# TODO create an Title Screen
+trtl.Screen().bgcolor("gray")
+title = trtl.Turtle()
+title.pensize(5)
+title.penup()
+title.goto(0,-150)
+title.write("Street Crossing!", align='center',  font=("Arial", 60, "normal"))
+
+# TODO Create Position for each cars
+
+# TODO Customize Turtle 
+
+# TODO Create an timer 
+
+# TODO Win or Lose Screen
+
+
+# TODO Players Scores and Difficulty level chosen
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
