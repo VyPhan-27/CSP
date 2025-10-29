af2db6555c232c6a9f9be169b4d99b7ec219fd35
VyPhan-27 <305681@shakopeeschools.org>
2025-10-28 21:26:03 -0500

    Setup background
    I had found an image that matched my game and also create an list

--- 1.2.5 - Shall We Play a Game/125_Vy.py
+++ 1.2.5 - Shall We Play a Game/125_Vy.py
@@ -1,24 +1,29 @@
 import turtle as trtl
 import random as rand
 
-
 # TODO create an Title Screen
-trtl.Screen().bgcolor("gray")
 title = trtl.Turtle()
 title.pensize(5)
 title.penup()
-title.goto(0,-150)
+title.goto(0,-110)
 title.write("Street Crossing!", align='center',  font=("Arial", 60, "normal"))
 # Create an street background
-title.goto(-300,0)
+screen = trtl.Screen()
+screen.setup(width=850, height=740)
+screen.title("Street Crossing!")
+screen.bgpic("street.gif")
 # TODO Create Position for each cars
+
+# Create an list
+cars = ["cars1","cars2","cars3","cars4","cars5","cars6"]
+cars_color = ["red","blue","green","black","grey","white"]
+# Custom Turtle
 
 # TODO Customize Turtle 
 
 # TODO Create an timer 
 
 # TODO Win or Lose Screen
-
 
 # TODO Players Scores and Difficulty level chosen
 
