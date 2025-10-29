5aa5c0e8d78fda6c02a41d410285249886b364c7
VyPhan-27 <305681@shakopeeschools.org>
2025-10-29 09:46:08 -0500

    Customize Turtle
    I begin by adding text input for when I connect those into my list afterwards and i started to position my car on the road

--- 1.2.5 - Shall We Play a Game/125_Vy.py
+++ 1.2.5 - Shall We Play a Game/125_Vy.py
@@ -13,15 +13,18 @@ screen.setup(width=850, height=740)
 screen.title("Street Crossing!")
 screen.bgpic("street.gif")
 # TODO Create Position for each cars
-
+starting_position = [140, 110, 80, 50, 20, 0]
 # Create an list
 cars = ["cars1","cars2","cars3","cars4","cars5","cars6"]
-cars_color = ["red","blue","green","black","grey","white"]
+default_cars_color = ["red","blue","green","black","grey","white"]
 # Custom Turtle
-
-# TODO Customize Turtle 
-
-# TODO Create an timer 
+car = ((-5,1),(-2,1),(-1,3),(3,3),(4,2),(4,1),(7,1),(7,-2),(4,-2),(4,-3),(3,-3),(3,-2),(-1,-2),(-1,-3),(-2,-3),(-3,-2),(-5,-2))
+# TODO Choice of Customize Turtle 
+customize = trtl.textinput("Start the game?","yes/no?")
+customize = trtl.textinput("Customize turtle", "Customize or Default?")
+customize = trtl.textinput("Turtle Colors", "Pick an Color")
+customize = trtl.textinput("Turtle Shapes", "Choose an Shape in Python")
+# TODO Create an timer
 
 # TODO Win or Lose Screen
 
